dfcd10c09a7864b99e6cb90f4d94d3631a4a023d
hongphipham97@gmail.com <hongphiham97@gmail.com>
2020-02-15 19:11:57 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,26 @@
+//   Given a list of numbers and a number k,
+//   return whether any two numbers from the list add up to k.
+//
+//   For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+/***
+ @param arr array of elements to check
+ @param sum
+ @return boolean
+***/
+function canArrayElmSumUpTo(arr, sum) {
+  const seen = new Map();
+
+  for (const element of arr) {
+
+    if (seen.has(sum - element)) {
+      return true
+    }
+
+    seen.set(element, true);
+  }
+
+  return false;
+}
+
+console.log(canArrayElmSumUpTo([10, 15, 3, 7], 17));
